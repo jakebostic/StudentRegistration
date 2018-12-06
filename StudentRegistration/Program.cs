5502b93fd6f1957aae0d3992d475a2980bd50f79
jakebostic <jake_bostic@yahoo.com>
2018-12-06 15:36:54 -0500

    Initial Commit

---
+++ StudentRegistration/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace HelloWorld
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Student Registration Form");
+            Console.WriteLine();
+
+            Console.WriteLine("Enter first name: ");
+            var fname = Console.ReadLine();
+            Console.WriteLine("Enter last name: ");
+            var lname = Console.ReadLine();
+            Console.WriteLine("Enter year of birth: ");
+            var bdate = Console.ReadLine();
+
+            var name = fname + " " + lname;
+            var pass = fname + "*" + bdate;
+
+            Console.WriteLine($"\nWelcome {name}!");
+            Console.WriteLine("\nYour registration is complete.");
+            Console.WriteLine($"\nYour temporary password is: {pass}");
+            Console.WriteLine();
+
+            Console.Write("\nPress any key to exit...");
+            Console.ReadKey(true);
+
+        }
+    }
+}
